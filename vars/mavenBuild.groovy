dd11706de34f810ac262f8fea7bdc454045049ca
Sebastian Hempel <shempel@it-hempel.de>
2025-03-13 13:16:32 +0100

    fix npe

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,5 +1,8 @@
 def call(Map params) {
-	def pom = params.get('pom', 'pom.xml')
+	def pom = 'pom.xml'
+	if (param != null) {
+		pom = params.get('pom', 'pom.xml')
+	}
 	echo "Building ${pom} ..."
 	sh "echo ${pom}"
 }
